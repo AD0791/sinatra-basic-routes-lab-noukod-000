3e14c6ece409b09e8eef46e68762c458a07b7101
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-29 15:52:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  # homepage
+  get '/' do
+    "Hello, World!"
+  end
+  # name
+  get '/name' do
+    "My name is "
+  end
+  # hometown
+  get '/hometown' do
+    "My hometown is "
+  end
+  # name
+  get '/favorite-song' do
+    "My favorite song is "
+  end
 end
